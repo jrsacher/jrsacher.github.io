c41b9eaf01d3a4283c59d8bf4845e8523f79f4fe
Joshua Sacher <joshuasacher@gmail.com>
2023-02-01 16:28:55 -0600

    decorator example

--- cs50/sections/section9/decorator.py
+++ cs50/sections/section9/decorator.py
@@ -1,5 +1,6 @@
 # This function returns another function!
 def add_one(func):
+    # "*args, **kwargs" is used in case the decorated function has arguments
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs) + 1
 
@@ -18,6 +19,13 @@ def decorated_one():
 # This is the same as above -- "two" is now a function that can be called
 two = add_one(one)
 
+
+@add_one
+def number(n: int):
+    return n
+
+
 print(one())  # 1
 print(decorated_one())  # 2
 print(two())  # 2
+print(number(50))  # 51
