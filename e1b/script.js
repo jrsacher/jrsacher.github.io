2ff5fa3aa5e7f7cf29cb7ebe2031fff1bcac628f
Joshua Sacher <joshuasacher@gmail.com>
2023-10-26 18:50:21 -0500

    fixing examples

--- e1b/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-  const text = document.getElementById('switch');
-  text.onmouseover = function () {
-    text.setAttribute('class', 'ugly');
-  };
-  text.onmouseout = function () {
-    text.setAttribute('class', 'pretty');
-  };
-};
