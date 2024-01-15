f46824ea70cca31f8c2ac538f354acf49b37597f
Joshua Sacher <31259475+jrsacher@users.noreply.github.com>
2024-01-15 10:25:39 -0600

    Update lambda.py to use CSV

--- cs50/sections/section7/lambda.py
+++ cs50/sections/section7/lambda.py
@@ -1,45 +1,28 @@
 # Example of lambda ("anonymous" or unnamed) functions --
 # Short, single-use functions
+import csv
 
-def main():
-    # Print sorted by first name
-    for name in sorted(STAFF):
-        print(name)
+with open("staff_2024.csv", "r") as f:
+    reader = csv.DictReader(f)
+    staff = []
+    for row in reader:
+        staff.append(row["name"])
 
-    print()
+# Print staff list as-is
+for name in staff:
+    print(name)
+print()
 
-    # Print sorted by last name
-    for name in sorted(STAFF, key=lambda s: s.split(" ")[-1]):
-        print(name)
+# Print sorted by first name
+for name in sorted(staff):
+    print(name)
+print()
+
+# Print sorted by last name
+for name in sorted(staff, key=lambda s: s.split(" ")[-1]):
+    print(name)
 
 # lambda s: s.split(" ")[-1]
 # is equivalent to
 # def last_name(s):
 #   return s.split(" ")[-1]
-
-STAFF = [
-    "David Malan",
-    "Doug Lloyd",
-    "Bernie Longboy",
-    "Brenda Anderson",
-    "Glenn Holloway",
-    "Catherine Deskur",
-    "Josh Archibald",
-    "Josh Sacher",
-    "Tim Sampson",
-    "Athena Braun",
-    "Guy White",
-    "Thomas Ballatore",
-    "Taha Teke",
-    "Haley Mosdell",
-    "Shaanan Cohney",
-    "Varsha Venkatasubramanian",
-    "Margaret Tanzosh",
-    "Ni'mah Faadlilah",
-    "Bradley Ross",
-    "Zad Chin",
-]
-
-
-if __name__ == "__main__":
-    main()
